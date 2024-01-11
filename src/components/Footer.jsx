2198194f65560ba4d8ee24d72843e13ab99a71eb
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-01-11 23:26:22 +0800

    fixed suggested color in nav button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 
-
 export default function Footer() {
   return (
     <footer className="bg-gray-800">
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -29,7 +29,7 @@ export default function Navigation() {
               <li key={item.id}>
                 <a
                   href={item.id}
-                  className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-blue-700 px-3 py-2 rounded-md text-sm font-medium"
                 >
                   {item.label}
                 </a>
